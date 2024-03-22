cdbb2cd0e7e7de077f5b790a529bd1c6af988704
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-22 21:57:39 +0100

    add header and fix input search width, fix layout between these two elements

--- logicgate_visitors_blog/src/components/components.ts
+++ logicgate_visitors_blog/src/components/components.ts
@@ -4,6 +4,7 @@ export { default as Logo } from "./ui/logo/Logo";
 export { default as ListElement } from "./ui/list_element/ListElement";
 export { default as TagElement } from "./ui/tag_element/TagElement";
 export { default as InputElement } from "./ui/input_element/InputElement";
+export { default as HeaderElement } from "./ui/header_element/HeaderElement";
 
 /* Layout */
 export { default as NavbarLayout } from "./navbar_layout/NavbarLayout";

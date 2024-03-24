b854bc91b7b0ed9d72e9408d18fee53f3684d765
Eduardo Hernandez <eduardo@chatbot-plus.com>
2024-03-24 10:05:49 +0100

    add logo routing to home

--- logicgate_visitors_blog/src/components/navbar_layout/NavbarLayout.tsx
+++ logicgate_visitors_blog/src/components/navbar_layout/NavbarLayout.tsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
 import { HeaderElement, Logo } from "../components";
 import styles from "./navbar_layout.module.scss";
 
 const NavbarLayout = () => {
   return (
-    <div className={styles.navbar}>
-      <Logo className={styles.navbar__logo} />
-      <HeaderElement title="Logic gates" level={2} />
+    <div>
+      <Link
+        className={styles.navbar}
+        style={{
+          textDecoration: "none",
+          color: "inherit",
+        }}
+        to={"/"}
+      >
+        <Logo className={styles.navbar__logo} />
+        <HeaderElement title="Logic gates" level={2} />
+      </Link>
     </div>
   );
 };
